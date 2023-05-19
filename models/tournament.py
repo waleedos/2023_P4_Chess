6a00dc25afd5e3be7c673d8ecf79741182b724df
EL-WALID EL-KHABOU <alkhabou@gmail.com>
2023-05-19 16:17:12 +0200

    Ajout des test pour JSON

--- models/tournament.py
+++ models/tournament.py
@@ -1,16 +1,12 @@
 import json
-from datetime import datetime
-from models.round import Round
-
+import datetime
 
 class Tournament:
-    DATE_FORMAT = "%d/%m/%Y"
-
     def __init__(self, name, location, start_date, end_date, rounds=4):
         self.name = name
         self.location = location
-        self.start_date = datetime.strptime(start_date, self.DATE_FORMAT)
-        self.end_date = datetime.strptime(end_date, self.DATE_FORMAT)
+        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y")
+        self.end_date = datetime.datetime.strptime(end_date, "%d/%m/%Y")
         self.rounds = rounds
         self.current_round = 0
         self.rounds_list = []
@@ -18,23 +14,25 @@ class Tournament:
         self.description = ''
 
     def to_json(self):
-        return {
+        data = {
             "name": self.name,
             "location": self.location,
-            "start_date": self.start_date.strftime(self.DATE_FORMAT),
-            "end_date": self.end_date.strftime(self.DATE_FORMAT),
+            "start_date": self.start_date.strftime("%d/%m/%Y"),
+            "end_date": self.end_date.strftime("%d/%m/%Y"),
             "rounds": self.rounds,
             "current_round": self.current_round,
             "rounds_list": [round.to_json() for round in self.rounds_list],
             "players_list": [player.to_json() for player in self.players_list],
-            "description": self.description,
+            "description": self.description
         }
+        return json.dumps(data)
 
     @classmethod
     def from_json(cls, json_str):
         data = json.loads(json_str)
-        data["start_date"] = datetime.strptime(data["start_date"], cls.DATE_FORMAT)
-        data["end_date"] = datetime.strptime(data["end_date"], cls.DATE_FORMAT)
-        data["rounds_list"] = [Round.from_json(round) for round in data["rounds_list"]]
-        data["players_list"] = [Player.from_json(player) for player in data["players_list"]]
-        return cls(**data)
+        tournament = cls(data["name"], data["location"], data["start_date"], data["end_date"], data["rounds"])
+        tournament.current_round = data["current_round"]
+        tournament.rounds_list = [Round.from_json(round_json) for round_json in data["rounds_list"]]
+        tournament.players_list = [Player.from_json(player_json) for player_json in data["players_list"]]
+        tournament.description = data["description"]
+        return tournament
