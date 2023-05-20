f95ac6ead3b2c91800d254f254ca37969447bea7
EL-WALID EL-KHABOU <alkhabou@gmail.com>
2023-05-20 11:53:04 +0200

    Effacement du probleme commis par JSON

--- controllers/player_controller.py
+++
@@ -1,5 +0,0 @@
-from models.player import Player
-
-def create_player(last_name, first_name, date_of_birth, score):
-    player = Player(last_name, first_name, date_of_birth, score)
-    return player
--- models/player.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-class Player:
-    def __init__(self, last_name, first_name, date_of_birth, score):
-        self.last_name = last_name
-        self.first_name = first_name
-        self.date_of_birth = datetime.strptime(date_of_birth, "%d/%m/%Y").strftime("%d/%m/%Y")
-        self.score = score
