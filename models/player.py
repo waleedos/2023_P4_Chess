b9509da579d9d46be509b10f255d6b8b7b7da405
EL-WALID EL-KHABOU <alkhabou@gmail.com>
2023-05-21 16:29:46 +0200

    Refonte de presque tout le projet

--- models/player.py
+++
@@ -1,6 +0,0 @@
-class Player:
-    def __init__(self, last_name, first_name, date_of_birth, score):
-        self.last_name = last_name
-        self.first_name = first_name
-        self.date_of_birth = date_of_birth
-        self.score = score
